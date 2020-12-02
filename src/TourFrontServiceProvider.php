6a0de3747f482eeeead83ecb8cd5a1eb00d6fb27
Budi <aku@budi.my.id>
2020-12-02 12:15:44 +0700

    #1891

--- src/TourFrontServiceProvider.php
+++ src/TourFrontServiceProvider.php
@@ -26,7 +26,6 @@ class TourFrontServiceProvider extends ServiceProvider
         $this->loadViewsFrom(__DIR__.'/views', 'tourfront');
         
         $this->publishes([ __DIR__.'/publish/manifest' => public_path(''),], 'public');
-        $this->publishes([ __DIR__.'/publish/assets' => public_path('assets'),], 'public');
         $this->publishes([ __DIR__.'/publish/css' => public_path('css'),], 'public');
         $this->publishes([ __DIR__.'/publish/js' => public_path('js'),], 'public');
         $this->publishes([ __DIR__.'/publish/img' => public_path('img'),], 'public');
