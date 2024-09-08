44d0db4d6909f1838d0c53e5d81bcb24453a7137
Budi <aku@budi.my.id>
2024-09-09 01:23:05 +0700

    Update APIController.php

--- src/Controllers/APIController.php
+++ src/Controllers/APIController.php
@@ -45,14 +45,7 @@ class APIController extends Controller
 
     public function test(Request $request)
     {
-        $shoppingcart = Shoppingcart::where('session_id','1b3fb63-1c0-f550-c20e-cdcedaa645b7')->where('confirmation_code','XDT240909009')->first();
-        print_r($shoppingcart);
-            $email = $shoppingcart->shoppingcart_questions()->select('answer')->where('type','mainContactDetails')->where('question_id','email')->first()->answer;
-            if($email!="")
-            {
-                Mail::to($email)->cc([env("MAIL_FROM_ADDRESS")])->send(new BookingConfirmedMail($shoppingcart));
-            }
-            return response('OK1', 200)->header('Content-Type', 'text/plain');
+        
     }
 
     public function cancellation($sessionId,$confirmationCode)
