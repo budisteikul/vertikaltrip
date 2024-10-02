4cd9678cf5f7937d7f4647d3806fcdaba985fb26
Budi <aku@budi.my.id>
2024-10-02 13:19:06 +0700

    Update WebhookController.php

--- src/Controllers/WebhookController.php
+++ src/Controllers/WebhookController.php
@@ -135,7 +135,7 @@ class WebhookController extends Controller
                 //==================================================
                 switch(strtolower($message))
                 {
-                    case "/schedule":
+                    case "/participanttoday":
                         $message = BookingHelper::schedule_bydate(date('Y-m-d'));
                         $whatsapp->sendText($from,$message);
                     break;
