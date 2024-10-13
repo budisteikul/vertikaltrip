b0e3ac8001a103b408050eb69991d072207ffa5a
Budi <aku@budi.my.id>
2024-10-13 12:11:35 +0700

    Update APIController.php

--- src/Controllers/APIController.php
+++ src/Controllers/APIController.php
@@ -91,7 +91,7 @@ class APIController extends Controller
         
         $analytic = LogHelper::analytic();
 
-        if(str_contains(GeneralHelper::url(), 'jogjafoodtour'))
+        if(str_contains(GeneralHelper::url(), 'jogjafoodtour') str_contains(GeneralHelper::url(), 'vertikaltrip'))
         {
 
             
