c57a61bf0a70aea48d9a7e6d2be7d8b3f8e1432b
Budi <aku@budi.my.id>
2025-04-08 18:05:40 +0700

    Update 2025_04_08_172809_create_orders_table.php

--- src/migrations/2025_04_08_172809_create_orders_table.php
+++ src/migrations/2025_04_08_172809_create_orders_table.php
@@ -13,8 +13,9 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->string('products')->nullable();
-            $table->string('guides')->nullable();
+            $table->date('date')->nullable();
+            $table->string('product')->nullable();
+            $table->string('guide')->nullable();
             $table->text('transactions');
             $table->timestamps(6);
         });
