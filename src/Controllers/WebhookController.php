7f5349329f9649e901aee22f8c24765db4334360
Budi <aku@budi.my.id>
2025-07-30 13:35:36 +0700

    Update WebhookController.php

--- src/Controllers/WebhookController.php
+++ src/Controllers/WebhookController.php
@@ -94,10 +94,13 @@ class WebhookController extends Controller
                 }
                 else
                 {
+                    $price = BookingHelper::convert_currency($content->nextDefaultPriceMoney->amount,config('site.currency'),$currency);
+                    $total_price = $price * $decryptedData["decryptedBody"]["data"]["participant"];
+
                     //summary
                     if($payment!="")
                     {
-                        $body_information = "Pay online";
+                        $body_information = $decryptedData["decryptedBody"]["data"]["participant"] ." x ".$currency." ". ($price * 1);
                         $payment="on";
                     }
                     else
@@ -107,8 +110,7 @@ class WebhookController extends Controller
                     }
 
                     
-                    $price = BookingHelper::convert_currency($content->nextDefaultPriceMoney->amount,config('site.currency'),$currency);
-                    $total_price = $price * $decryptedData["decryptedBody"]["data"]["participant"];
+                    
                     
                     $more_details = 'no dietary';
                     if(isset($decryptedData["decryptedBody"]["data"]["more_details"])) $more_details = $decryptedData["decryptedBody"]["data"]["more_details"];
