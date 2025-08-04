83aa6e63072619f349054796e9648df3649fd411
Budi <aku@budi.my.id>
2025-08-04 12:42:01 +0700

    Update PaymentController.php

--- src/Controllers/PaymentController.php
+++ src/Controllers/PaymentController.php
@@ -754,7 +754,7 @@ class PaymentController extends Controller
                                     $(\'#alert-payment\').html(\'<div id="alert-success" class="alert alert-primary text-center" role="alert"><h2 style="margin-bottom:10px; margin-top:10px;"><i class="far fa-smile"></i> Payment Successful!</h2></div>\');
                                     $(\'#alert-payment\').fadeIn("slow");
                                     setTimeout(function (){
-                                        window.openAppRoute("/payment/page/payment-successful");
+                                        window.openAppRoute("/payment/successful");
                                     }, 1000); 
                                 }
 
@@ -839,7 +839,7 @@ class PaymentController extends Controller
                                     $(\'#alert-payment\').html(\'<div id="alert-success" class="alert alert-primary text-center" role="alert"><h2 style="margin-bottom:10px; margin-top:10px;"><i class="far fa-smile"></i> Payment Successful!</h2></div>\');
                                     $(\'#alert-payment\').fadeIn("slow");
                                     setTimeout(function (){
-                                        window.openAppRoute("/payment/page/payment-successful");
+                                        window.openAppRoute("/payment/successful");
                                     }, 1000); 
                                 }
 
@@ -869,7 +869,7 @@ class PaymentController extends Controller
                                     $(\'#alert-payment\').html(\'<div id="alert-success" class="alert alert-primary text-center" role="alert"><h2 style="margin-bottom:10px; margin-top:10px;"><i class="far fa-smile"></i> Payment Successful!</h2></div>\');
                                     $(\'#alert-payment\').fadeIn("slow");
                                     setTimeout(function (){
-                                        window.openAppRoute("/payment/page/payment-successful");
+                                        window.openAppRoute("/payment/successful");
                                     }, 1000); 
                                 }
 
