09d7e4999bc09aaa70d20cd5ed7f5018c39ef71e
Budi <aku@budi.my.id>
2025-09-05 12:59:56 +0700

    Update WebhookController.php

--- src/Controllers/WebhookController.php
+++ src/Controllers/WebhookController.php
@@ -65,7 +65,8 @@ class WebhookController extends Controller
                                             ];
             
                                             $whatsapp = new WhatsappHelper;
-                                            $whatsapp->sendTemplate("6285743112112","online_payment", $components);
+                                            $aaa = $whatsapp->sendTemplate("6285743112112","online_payment", $components);
+                                            print_r($aaa);
             exit();
         }
 
