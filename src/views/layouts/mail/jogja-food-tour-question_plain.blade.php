0f5657a4813270f40111300f35f6eb2ecc86b3bf
Budi <aku@budi.my.id>
2026-06-16 19:57:14 +0700

    1

--- src/views/layouts/mail/jogja-food-tour-question_plain.blade.php
+++ src/views/layouts/mail/jogja-food-tour-question_plain.blade.php
@@ -1,9 +1,10 @@
-Hello {{$data->name}} 👋 
+Hello {{$data->name}} 👋
+<br> 
 Thank you for booking our food tour 
-
+<br><br>
 The food tour will start {{ $data->time_description }} at {{ $data->time }} and our meeting point is at {{ $data->location }}.
-
+<br><br>
 Map : {{$data->map}}
 {{$data->map_description}}
-
+<br><br>
 By the way, do you/does anyone in your group have any food allergy or dietary restrictions?
--- src/views/layouts/mail/thank-you-for-booking_plain.blade.php
+++ src/views/layouts/mail/thank-you-for-booking_plain.blade.php
@@ -1,11 +1,11 @@
 Dear {{$data->name}} 👋,
-
+<br><br>
 Thank you for booking a tour with us! We are delighted to have you join us and look forward to sharing a wonderful experience with you.
-
+<br><br>
 To help us prepare for your tour, could you please let us know if you/anyone in your group have any food allergies or dietary restrictions that we should be aware of?
-
+<br><br>
 We appreciate your response and look forward to welcoming you soon.
-
+<br><br>
 Kind regards,
-
+<br><br>
 Jogja Food Tour Team
