da15625e4a550b91b1323d72deb9b646961386df
Budi <aku@budi.my.id>
2026-06-17 01:42:56 +0700

    1

--- src/views/layouts/mail/jogja-food-tour-question_plain.blade.php
+++ src/views/layouts/mail/jogja-food-tour-question_plain.blade.php
@@ -1,11 +1,15 @@
-Hello {{$data->name}} 👋
-<br> 
-Thank you for booking our food tour 
-<br><br>
-The food tour will start {{ $data->time_description }} at {{ $data->time }} and our meeting point is at {{ $data->location }}.
-<br><br>
-Map : {{$data->map}}
-<br>
-{{$data->map_description}}
-<br><br>
-By the way, do you/does anyone in your group have any food allergy or dietary restrictions?
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Jogja Food Tour</title>
+</head>
+<body>
+    <p>Hello {{$data->name}} 👋,</p>
+    <p>Thank you for booking our food tour .</p>
+    <p>The food tour will start {{ $data->time_description }} at {{ $data->time }} and our meeting point is at {{ $data->location }}.</p>
+    <p>Map : {{$data->map}}</p>
+    <p>{{$data->map_description}}</p>
+    <p>By the way, do you/does anyone in your group have any food allergy or dietary restrictions?</p>
+</body>
+</html>
--- src/views/layouts/mail/thank-you-for-booking_plain.blade.php
+++ src/views/layouts/mail/thank-you-for-booking_plain.blade.php
@@ -1,11 +1,14 @@
-Dear {{$data->name}} 👋,
-<br><br>
-Thank you for booking a tour with us! We are delighted to have you join us and look forward to sharing a wonderful experience with you.
-<br><br>
-To help us prepare for your tour, could you please let us know if you/anyone in your group have any food allergies or dietary restrictions that we should be aware of?
-<br><br>
-We appreciate your response and look forward to welcoming you soon.
-<br><br>
-Kind regards,
-<br><br>
-Jogja Food Tour Team
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Jogja Food Tour</title>
+</head>
+<body>
+    <p>Dear {{$data->name}} 👋,</p>
+    <p>Thank you for booking a tour with us! We are delighted to have you join us and look forward to sharing a wonderful experience with you.</p>
+    <p>To help us prepare for your tour, could you please let us know if you/anyone in your group have any food allergies or dietary restrictions that we should be aware of?</p>
+    <p>We appreciate your response and look forward to welcoming you soon.</p>
+    <p>Kind regards,</p>
+    <p>Jogja Food Tour Team</p>
+</body>
+</html>
