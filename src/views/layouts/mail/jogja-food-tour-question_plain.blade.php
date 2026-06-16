06f21fc35a30eb8c465c5191bf3c5c7fa0616058
Budi <aku@budi.my.id>
2026-06-16 20:04:47 +0700

    Update jogja-food-tour-question_plain.blade.php

--- src/views/layouts/mail/jogja-food-tour-question_plain.blade.php
+++ src/views/layouts/mail/jogja-food-tour-question_plain.blade.php
@@ -5,6 +5,7 @@ Thank you for booking our food tour
 The food tour will start {{ $data->time_description }} at {{ $data->time }} and our meeting point is at {{ $data->location }}.
 <br><br>
 Map : {{$data->map}}
+<br>
 {{$data->map_description}}
 <br><br>
 By the way, do you/does anyone in your group have any food allergy or dietary restrictions?
