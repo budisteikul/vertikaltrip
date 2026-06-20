c218f1d7a8b28831f11bf4b7cafe8e1090672b2a
Budi <aku@budi.my.id>
2026-06-20 09:40:45 +0700

    Update jogja-food-tour-question_plain.blade.php

--- src/views/layouts/mail/jogja-food-tour-question_plain.blade.php
+++ src/views/layouts/mail/jogja-food-tour-question_plain.blade.php
@@ -10,13 +10,13 @@
 
 <p>Just a friendly reminder that your food tour is scheduled for <b>{{ $data->time_description }}</b>.</p>
 
-<p>
-<div>📅 Date: <b>{{ $data->date }}</b></div>
-<div>🕒 Time: <b>{{ $data->time }}</b></div>
-<div>📍 Meeting Point: <b>{{ $data->location }}</b></div>
-<div>🗺️ Maps: <b>{{ $data->map }}</b></div>
-<div><i>{{$data->map_description}}</i></div>
-</p>
+
+<p>📅 Date: <b>{{ $data->date }}</b></p>
+<p>🕒 Time: <b>{{ $data->time }}</b></p>
+<p>📍 Meeting Point: <b>{{ $data->location }}</b></p>
+<p>🗺️ Maps: <b>{{ $data->map }}</b></p>
+<p><i>{{$data->map_description}}</i></p>
+
 
 <p>{{$data->question}}</p>
 
